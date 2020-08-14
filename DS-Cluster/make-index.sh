74ade23ce3400d1ae2272d21735c422fee2637b8
Alex Root <newrou@mail.ru>
2020-08-14 18:08:54 +0700

    Add data for DC Cluster

---
+++ DS-Cluster/make-index.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "<html>" > index.html
+echo "<head><meta charset=\"utf-8\"/></head>" >> index.html
+echo "<body>" >> index.html
+echo "<h3><a href="../index.html">Quantm-chemical calculations of diazonium salts</a></h3>" >> index.html
+echo "<h4>Structures of cluster DS in LC-MS experiment:</h4>" >> index.html
+
+echo "<ul>" >> index.html
+list=`find . -maxdepth 1 -mindepth 1 -type d -printf "%f\n" | sort`
+for i in $list
+do
+  echo " <li><a href=\"$i\">$i</a></li>" >> index.html
+  cd $i
+  ./make-index.sh
+  cd ..
+done
+echo "</ul>" >> index.html
+
+echo "<ul>" >> index.html
+list=`ls *.out`
+for i in $list
+do
+  name=`basename $i .out`
+  echo " <li>$name <a href=\"$name.out\">out</a> <a href=\"$name.xyz\">xyz</a> <a href=\"$name.dat\">dat</a> </li>" >> index.html
+done
+echo "</ul>" >> index.html
+
+echo "</body>" >> index.html
+echo "</html>" >> index.html
--- make-index.sh
+++ make-index.sh
@@ -6,6 +6,7 @@ echo "<body>" >> index.html
 echo "<h3>Quantm-chemical calculations of diazonium salts</h3>" >> index.html
 echo "<h4><a href=\"DS/index.html\">Structures of diazonium salts</a></h4>" >> index.html
 echo "<h4><a href=\"TS/index.html\">Transition state of the de-diazonation reaction of diazonium salts</a></h4>" >> index.html
+echo "<h4><a href=\"DS-Cluster/index.html\">Structures of cluster DS in LC-MS experiment</a></h4>" >> index.html
 
 #echo "<ul>" >> index.html
 list=`find . -maxdepth 1 -mindepth 1 -type d -printf "%f\n" | sort`
